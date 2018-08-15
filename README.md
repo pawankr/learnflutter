b21b8be7bf2bb7fe9379048e2cf65652be2062e8
Pawan Dontula <Pawan.Dontula@emc.com>
2018-08-15 13:05:15 +0100

    async await Decoration Container Stateful widgets

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ A new Flutter project.
 
 For help getting started with Flutter, view our online
 [documentation](https://flutter.io/).
+
+## Run emulator from cmd line 
+
+cd C:\Users\dontup1\AppData\Local\Android\Sdk\emulator\
+.\emulator.exe -avd Nexus_5X_API_28
--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,29 +1,47 @@
 //Import flutter helper library
 
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
+import 'models/image_model.dart';
+import 'dart:convert';
+import 'widgets/image_list.dart';
+class App extends StatefulWidget{
+  @override
+    State<StatefulWidget> createState() {
+      return AppState();
+    }
 
-//Create a class that will be our custom widget
-//this class must extend the 'StatelessWidget' base class
-class App extends StatelessWidget {
-//Must define a 'build' method that returns the widgets that *this* widget will show
+}
+class AppState extends State<App> {
+  int counter = 0;
+  List<ImageModel> images = [];
+
+  void fetchImage() async {
+    counter++;
+    var response = await get('https://jsonplaceholder.typicode.com/photos/$counter');
+    var imageModel = ImageModel.fromJson(json.decode(response.body));
+
+    setState((){
+      images.add(imageModel);
+    });
+  }
+
   @override
     Widget build(BuildContext context) {
-      // TODO: implement build
       return MaterialApp(
         home: Scaffold(
-        floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.black,
-        child: Icon(Icons.casino),
-        onPressed: () {
-          print('Hi there!!');
-          },
-        ),
-        appBar: AppBar(
-          title: Text('Lets use some images!'),
+          body: ImageList(images),
+          floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.orange,
+          child: Icon(Icons.view_headline),
+          onPressed: fetchImage,
+          ),
+          appBar: AppBar(
+          title: Text('Lets see some images!'),
         ),
       ),
-    );
-  }  
+      );
+    }  
 }
 
 
